1fae8a84b77050e4eab435681c7cc7fa50904250
hanswalterja-cyber <hans.walter.ja@gmail.com>
2026-06-28 02:39:19 +0200

    Implement PWM controller for dynamic RGB LED eye expressions.

--- rgbLED.hpp
+++ rgbLED.hpp
@@ -12,7 +12,7 @@ void setupLED(){
     pinMode(bluePin, OUTPUT);
 }
 
-void setColor(int red, int green, int blue) {
+void setColor(int red, int green, int blue){
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);
